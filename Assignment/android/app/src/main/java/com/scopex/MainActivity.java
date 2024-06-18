24da42cda568d553cc0780efad82407d729b8296
Vijay Gojiya <vmgojiya32@gmail.com>
2024-06-19 00:38:40 +0530

    chore: updated temp android packages and bundle id name

--- Assignment/android/app/src/main/java/com/scopex/MainActivity.java
+++ Assignment/android/app/src/main/java/com/scopex/MainActivity.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.scopex;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- Assignment/android/app/src/main/java/com/scopex/MainApplication.java
+++ Assignment/android/app/src/main/java/com/scopex/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.scopex;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
