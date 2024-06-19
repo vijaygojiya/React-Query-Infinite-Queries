66a5867f4c4705db11f4148ab6ab4eb37aa8dd7b
Vijay Gojiya <vmgojiya32@gmail.com>
2024-06-19 11:23:19 +0530

    fix(firebase): firebase and google sign in IOS configuration

--- Assignment/ios/AwesomeProject/AppDelegate.mm
+++ Assignment/ios/AwesomeProject/AppDelegate.mm
@@ -2,12 +2,19 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import "RNBootSplash.h"
+#import <Firebase.h>
+ #import <GoogleSignIn/GoogleSignIn.h>
 
 @implementation AppDelegate
 
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+  return [GIDSignIn.sharedInstance handleURL:url];
+}
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
   self.moduleName = @"AwesomeProject";
+  [FIRApp configure];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
